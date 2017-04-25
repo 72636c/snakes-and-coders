deab460a32460e76dee2ab0e6cab0215c7518461
Ryan Ling <ryan@outlook.com.au>
2017-04-25 14:08:05 +1000

    Add Python environment (#1)
    * Ignore node modules
    
    * Add Lambda function
    
    * Add primary site assets
    
    * Add redirect site config
    
    * Document deployment in README
    
    * Clean up stylesheet
    
    * Clean up page layout and scripting
    
    * Add project overview to README
    
    * Make JSLint happy-ish

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
 # Snakes and Coders
+
+Coding 101 with Python 3.
+
+## Overview
+
+`Snakes and Coders` provides a simple, web-based Python "environment" to get started with coding.
+
+It consists of:
+
+- A static website with a basic code editor and facilities to submit the code for execution
+- An API that runs arbitrary Python code and returns execution details
+
+**<https://www.snakesandcoders.com/>**
+
+## Deployment
+
+### Prerequisites
+
+Sign up for an [**AWS**](https://aws.amazon.com/) account.
+
+Install [**Node.js and npm**](https://nodejs.org/).
+
+### Lambda API
+
+#### Create Lambda function
+
+Upload [**lambda/lambda_function.py**](lambda/lambda_function.py) to AWS Lambda.
+
+#### Trigger with API Gateway
+
+Create resource with POST method to trigger the Lambda function.
+
+Create API key and associate with POST method.
+
+Enable CORS:
+
+Option                       | Value
+:--                          | :--
+Access-Control-Allow-Headers | 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Invocation-Type,X-Amz-Security-Token'
+Access-Control-Allow-Origin  | '*'
+
+Deploy API.
+
+### Static website
+
+#### Register custom domain
+
+Register domain name, e.g. `domain.tld`, via Route 53.
+
+Request SSL certificates `domain.tld` and `www.domain.tld` with ACM.
+
+#### Package assets
+
+Install Monaco Editor:
+
+```bash
+cd s3
+npm install monaco-editor@0.8.3
+```
+
+Update AJAX request in [**s3/main.js**](s3/main.js) to specify the correct `X-Api-Key` header and `url` for the Lambda API.
+
+Update [**s3-nonwww/index.html**](s3-nonwww/index.html) and [**s3-nonwww/redirection.xml**](s3-nonwww/redirection.xml) to point to `https://www.domain.tld`.
+
+#### Host assets on S3
+
+Create primary S3 bucket, e.g. `domain`:
+
+- Upload contents of the [**s3**](s3) directory
+- Configure for static website hosting
+- Configure public permissions
+
+Create redirect S3 bucket, e.g. `domain-nonwww`:
+
+- Upload [**s3-nonwww/index.html**](s3-nonwww/index.html)
+- Configure for static website hosting
+- Configure redirection rules per [**s3-nonwww/redirection.xml**](s3-nonwww/redirection.xml)
+- Configure public permissions
+
+#### Publish to custom domain via CDN
+
+Create primary CloudFront distribution:
+
+- CNAME: `www.domain.tld`
+- Origin: `domain.s3.amazonaws.com`
+- SSL Certificate: `www.domain.tld`
+- Viewer Protocol Policy: Redirect HTTP to HTTPS
+
+Create redirect CloudFront distribution:
+
+- CNAME: `domain.tld`
+- Origin: `domain-nonwww.s3-website-<region>.amazonaws.com`
+- SSL Certificate: `domain.tld`
+- Viewer Protocol Policy: Redirect HTTP to HTTPS
+
+Create Route 53 hosted zone record sets:
+
+- `www.domain.tld`: A and AAAA aliased to primary CloudFront distribution
+- `domain.tld`: A and AAAA aliased to redirect CloudFront distribution
---
+++ s3/main.js
@@ -0,0 +1,93 @@
+/*global
+    $, console, monaco, require, window
+*/
+
+// Python API endpoint.
+var API_URL = "https://rqbnzw6j8k.execute-api.ap-southeast-2.amazonaws.com/prod/arbitrary-execution";
+var API_KEY = "OGOkeZSQ1S8w6sJDtfAHq5MvUqUqLPTyywIIhUO6";
+
+// Remove newline from the end of a string.
+function trimTrailingNewline(text) {
+    "use strict";
+    return text.replace(/\n$/, "");
+}
+
+// Convert locals dictionary to multi-line string.
+function stringifyLocals(locals) {
+    "use strict";
+    var result = "";
+    $.each(locals, function (key, value) {
+        result += key + " = " + JSON.stringify(value) + "\n";
+        return;
+    });
+    return trimTrailingNewline(result);
+}
+
+// Handle JSON response from AJAX request.
+function handleResponse(data) {
+    "use strict";
+    $("div#code-exec-response").text(trimTrailingNewline(data.response)).trigger("change");
+    $("div#code-exec-stdout").text(trimTrailingNewline(data.stdout)).trigger("change");
+    $("div#code-exec-locals").text(stringifyLocals(data.locals)).trigger("change");
+    $("input#code-editor-run").prop("disabled", false);
+}
+
+// Run when DOM is ready for execution.
+$(document).ready(function () {
+    "use strict";
+    // Get initial input.
+    var editorValue = window.localStorage.getItem("editorValue");
+    if (editorValue === null) {
+        editorValue = $("code#code-editor-value").text();
+        console.log(editorValue);
+    }
+    // Set up the Monaco Editor.
+    require.config({paths: {"vs": "node_modules/monaco-editor/min/vs"}});
+    require(["vs/editor/editor.main"], function () {
+        var editor = monaco.editor.create(document.getElementById("code-editor-container"), {
+            value: editorValue,
+            theme: "vs-dark",
+            language: "python",
+            automaticLayout: true
+        });
+        // Auto-save input every second.
+        window.setInterval(function () {
+            window.localStorage.setItem("editorValue", editor.getValue());
+        }, 1000);
+    });
+});
+
+// Submit form when Cmd/Ctrl + F9 is hit.
+$(document).keyup(function (event) {
+    "use strict";
+    if ((event.ctrlKey || event.metaKey) && (event.keyCode === 120)) {
+        event.stopPropagation();
+        $("form#code-editor-form").submit();
+    }
+});
+
+// Process code when form is submitted.
+$("form#code-editor-form").submit(function (event) {
+    "use strict";
+    event.preventDefault();
+    $("input#code-editor-run").prop("disabled", true);
+    $.ajax({
+        contentType: "text/plain",
+        data: monaco.editor.getModels()[0].getValue(),
+        processData: false,
+        type: "POST",
+        url: API_URL,
+        beforeSend: function (request) {
+            request.setRequestHeader("X-Api-Key", API_KEY);
+            request.setRequestHeader("X-Amz-Invocation-Type", "Event");
+        },
+        success: function (data) {
+            handleResponse(data);
+            console.log(data);
+        },
+        error: function (xhr) {
+            handleResponse(xhr.responseJSON);
+            console.log(xhr);
+        }
+    });
+});
